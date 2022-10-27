a7a67a4d83e5b68931cd388a9a1816b34f507c4a
nivalf <flavinleejohn@gmail.com>
2022-10-27 20:40:10 +0100

    Explore arena v1.0.0 : simple logic

---
+++ navigation_win/solutions/ea_1_0_0.c
@@ -0,0 +1,184 @@
+/* Explore Arena v1.0.0
+ *
+ * Authors:
+ * Flavin Lee John
+ * Ankur Gulia
+ *
+ * Date: 27 Oct 2022
+ *
+ * Algorithm:
+ * > Move forward
+ * > If obstacle Detected, turn right
+ * > If obstacle clear, move forward
+ *
+ * Possible Issues:
+ *
+ * 1. Bot might vibrate
+ * The threshold IR value to determine obstacle ahead and path clear is the same.
+ * Thus the bot frequently switches b/w the two states causing vibration.
+ *
+ * Sol: Keep the threshold IR value to stop turning lower than the one to stop moving forward.
+ *
+ * 2. Incomplete exploration
+ * Bot only turns to right which will result in incomplete exploration
+ *
+ * Sol: Identify which side is more open by comparing sensor values and move there.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+#include <main.h>
+
+// header files for UART
+#include "epuck1x/uart/e_uart_char.h"
+#include "stdio.h"
+#include "serial_comm.h"
+
+#include "sensors/proximity.h"
+#include "motors.h"
+
+// Define inter process communication bus
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
+
+// Function Declarations
+void move_forward(void);
+int obstacle_ahead(void);
+void turn_right(void);
+void should_stop_move_forward(void);
+void should_stop_turn(void);
+
+/* Bot State:
+ * 0: move forward mode
+ * 1: turn mode */
+int bot_state = 0;
+
+// Speed
+const int turn_speed = 200;
+const int move_speed = 800;
+
+int main(void)
+{
+    halInit();
+    chSysInit();
+    mpu_init();
+
+    motors_init();
+
+	// Initiate inter-process communication bus
+	messagebus_init(&bus, &bus_lock, &bus_condvar);
+
+    // Start & Calibrate the proximity sensor
+    proximity_start();
+    calibrate_ir();
+
+    // initialize UART1 channel
+    serial_start();
+
+    /* Infinite loop. */
+    while (1) {
+    	// delay in milliseconds. 10Hz
+        chThdSleepMilliseconds(100);
+
+		// DEV feedback
+		char str[100];
+		int str_length = sprintf(str, "bot_state: %d \n", bot_state);
+		e_send_uart1_char(str, str_length);
+
+        switch(bot_state) {
+        	// moving forward mode
+    		case 0:
+    			move_forward();
+    			should_stop_move_forward();
+    			break;
+
+    		// Turn mode
+    		case 1:
+    			turn_right();
+    			should_stop_turn();
+    			break;
+        }
+    }
+}
+
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
+
+/*************** Helper Functions *************************/
+
+// Move the bot forward;
+void move_forward(void) {
+	right_motor_set_speed(move_speed);
+	left_motor_set_speed(move_speed);
+}
+
+/* Switch bot state to turning mode if obstacle ahead*/
+void should_stop_move_forward(void) {
+	if(obstacle_ahead()) {
+		bot_state = 1;
+
+		// DEV feedback
+		char str[100]; // resulting string of sprintf will be stored here
+		int str_length = sprintf(str, "Obstacle detected. Switching to turn mode. \n");
+		e_send_uart1_char(str, str_length);
+	}
+}
+
+/**************** Turn ******************/
+
+// turn the bot clockwise
+void turn_right(void) {
+	right_motor_set_speed(-1 * turn_speed);
+	left_motor_set_speed(turn_speed);
+}
+
+/* Switch bot state to move forward mode if no obstacle ahead */
+void should_stop_turn(void) {
+	if(!obstacle_ahead()) {
+		bot_state = 0;
+
+		// DEV feedback
+		char str[100];
+		int str_length = sprintf(str, "Path clear. Switching to move forward mode. \n");
+		e_send_uart1_char(str, str_length);
+	}
+}
+
+/*********** Obstacle Detection *********************/
+
+/*	1 : obstacle ahead
+ *  0 : no obstacle ahead
+ */
+int obstacle_ahead(void) {
+	const int THR = 500;	// threshold value
+	int obstacle_detected = 0;
+
+	int sensors[4] = {0, 1, 6, 7};
+
+	for(int i=0; i<4; i++) {
+		if(get_prox(sensors[i]) > THR) {
+			obstacle_detected = 1;
+
+			// DEV feedback
+			char str[100];
+			char split = (i == 4) ? '\n' : '|';
+			int str_length = sprintf(str, " s%d: %d (%d) %c", i, get_prox(i), get_calibrated_prox(i), split);
+			e_send_uart1_char(str, str_length);
+		}
+	}
+
+	return obstacle_detected;
+}
---
+++ navigation_win/solutions/navigation_10_gyro.c
@@ -0,0 +1,357 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+#include <main.h>
+
+// header for proximity sensor
+#include "sensors/proximity.h"
+
+// header files for UART
+#include "epuck1x/uart/e_uart_char.h"
+#include "stdio.h"
+#include "serial_comm.h"
+
+#include "motors.h"
+
+#include "behaviors.h"
+#include "sensors/imu.h"
+#include "epuck1x/utility/utility.h"
+#include "selector.h"
+
+// Define inter process communication bus
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
+
+// Function Declarations
+void set_target_turn_angle(void);
+int cmp(const void *a, const void *b);
+int get_target_prox_sensor_number(void);
+void move_forward(void);
+int obstacle_in_proximity(void);
+void turn_right(void);
+void turn_left(void);
+void turn(void);
+void send_feedback_data(void);
+int get_turn_direction(void);
+int target_in_range(void);
+void change_sensor_select_count(void);
+void explore_arena(void);
+void chase_target(void);
+
+struct prox
+{
+    int value;
+    int sensor_no;
+};
+
+int sensor_select_count = 0;
+int bot_state = 0; 	// 0: moving fwd, 1: turning
+
+/* Angle for turning
+ *  +ve : turn right
+ *  -ve : turn left
+ */
+float target_turn_angle = 0.0;
+float current_turn_angle = 0.0;
+
+// Speed
+const int turn_speed = 200;
+const int move_speed = 800;
+
+int main(void)
+{
+    halInit();
+    chSysInit();
+    mpu_init();
+
+    motors_init();
+
+	// Initiate inter-process communication bus
+	messagebus_init(&bus, &bus_lock, &bus_condvar);
+
+    // Start & Calibrate the proximity sensor
+    proximity_start();
+
+	// Calibrate the sensors in use
+    calibrate_ir();
+	calibrate_acc();
+	calibrate_gyro();
+
+    // initialize UART1 channel
+    serial_start();
+
+    // Enable obstacle avoidance. Threshold: proximity < 300
+//	enable_obstacle_avoidance();
+
+
+    /* Infinite loop. */
+    while (1) {
+    	// waits
+        chThdSleepMilliseconds(100);
+        // Send feedback data to the serial monitor
+//        send_feedback_data();
+
+
+        switch(get_selector()) {
+        	case 0:
+        		// EXPLORATION MODE
+        		explore_arena();
+                break;
+			default:
+				// TARGET CHASE MODE
+				chase_target();
+        }
+    }
+}
+
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
+
+/*************** MAIN FUNCTIONS ***************************/
+
+// TASK 1: Explore the arena while avoiding collision
+void explore_arena(void) {
+    switch(bot_state) {
+    	// moving forward mode
+		case 0:
+			move_forward();
+			if(obstacle_in_proximity()) {
+				set_target_turn_angle();
+				change_sensor_select_count();
+				bot_state = 1;
+			}
+			break;
+
+		// Turn mode
+		case 1:
+			turn();
+			break;
+    }
+}
+
+// TASK 2: Chase a target
+void chase_target(void) {
+    switch(bot_state) {
+    	// moving forward mode
+		case 0:
+//        				move_forward();
+			if(target_in_range()) {
+				sensor_select_count = 0; // to select the closest one. Change to proper location
+				set_target_turn_angle();
+				bot_state = 1;
+			}
+			break;
+
+		// Turn mode
+		case 1:
+			turn();
+			break;
+    }
+}
+
+/*************** Helper Functions *************************/
+
+/*
+ * DEV
+ *
+ * - Add a reset function when selector state changes
+ * -
+ */
+
+// Move the bot forward;
+void move_forward(void) {
+	right_motor_set_speed(move_speed);
+	left_motor_set_speed(move_speed);
+}
+
+/**************** Turn ******************/
+
+void turn(void) {
+	int turn_direction = get_turn_direction();
+
+//	messagebus_topic_wait(imu_topic, &imu_values, sizeof(imu_values));
+	current_turn_angle += turn_direction * get_gyro_rate(2) * getDiffTimeMsAndReset() * 0.001;
+
+	char str[100]; // resulting string of sprintf will be stored here
+	int str_length = sprintf(str, "turn_direction: %d,current_turn_angle: %f, gyro_rate: %f, cacl value: %f \n", turn_direction,current_turn_angle, get_gyro_rate(2), turn_direction * get_gyro_rate(2) * getDiffTimeMsAndReset() * 0.001);
+	e_send_uart1_char(str, str_length);
+
+	if(abs(current_turn_angle) >= abs(target_turn_angle)) {
+		// reset
+		target_turn_angle = 0.0;
+		current_turn_angle = 0.0;
+		// set to move forward
+		bot_state = 0;
+	} else if (turn_direction == 1) {
+		turn_right();
+	} else if (turn_direction == -1) {
+		turn_left();
+	}
+}
+
+// turn the bot clockwise
+void turn_right(void) {
+	right_motor_set_speed(-1 * turn_speed);
+	left_motor_set_speed(turn_speed);
+}
+
+// turn the bot counter clockwise
+void turn_left(void) {
+	right_motor_set_speed(turn_speed);
+	left_motor_set_speed(-1 * turn_speed);
+}
+
+/* Set the target angle in radians to turn for next movement
+		 Position of the robot sensors:
+				forward
+
+		-M_PI/8			  7	  0 (15 deg) 		M_PI/8
+		-M_PI/4			6		1 (45 deg) 		M_PI/4
+		-M_PI/2		  5	    	  2 (90 deg) 	M_PI/2
+		-5*M_PI/4		 4	   3 (150 deg) 		5*M_PI/4
+*/
+void set_target_turn_angle(void) {
+	float sensor_angle_arr[8] = { M_PI/8.0, M_PI/4.0, M_PI/2.0, 5.0*M_PI/4.0, (-1.0)*M_PI/8.0, (-1.0)*M_PI/4.0, (-1.0)*M_PI/2.0, (-5.0)*M_PI/4.0 };
+	int target_sensor = get_target_prox_sensor_number();
+	target_turn_angle = sensor_angle_arr[target_sensor];
+
+	char str[100]; // resulting string of sprintf will be stored here
+	int str_length = sprintf(str, "target_sensor: %d, target_turn_angle: %f, sample_angle_value: %f \n", target_sensor, target_turn_angle, sensor_angle_arr[0]);
+	e_send_uart1_char(str, str_length);
+}
+
+/* Get the proximity sensor number of next direction */
+int get_target_prox_sensor_number(void) {
+	struct prox prox_values[8];
+	//int prox prox_values[8];
+
+	for (int sensor = 0; sensor < 8; sensor++)
+	    {
+	    	prox_values[sensor].value = get_prox(sensor);
+	    	prox_values[sensor].sensor_no = sensor;
+	    }
+
+	//sort
+	for (int i=0; i<8; i++)
+		for (int j=i+1; j<8; j++)
+		{
+			if (prox_values[i].value < prox_values[j].value) {
+				int tmp_v = prox_values[i].value;
+				prox_values[i].value = prox_values[j].value;
+				prox_values[j].value = tmp_v;
+				int tmp_n = prox_values[i].sensor_no;
+				prox_values[i].sensor_no = prox_values[j].sensor_no;
+				prox_values[j].sensor_no = tmp_n;
+			}
+		}
+
+	//qsort(prox_values, 8, sizeof(prox_values[0]), cmp2);
+
+
+	return prox_values[sensor_select_count].sensor_no;
+}
+
+
+// jump sequence
+void change_sensor_select_count(void) {
+	sensor_select_count = (sensor_select_count + 3) % 8;
+}
+
+/* Compare function for sort
+ * Sorts in descending order of sensor values => increasing order of distance
+ */
+int cmp(const void *a, const void *b) {
+    struct prox *a1 = (struct prox *)a;
+    struct prox *a2 = (struct prox *)b;
+    if ((*a1).value > (*a2).value)
+        return -1;
+    else if ((*a1).value < (*a2).value)
+        return 1;
+    else
+        return 0;
+}
+
+int cmp2(const int* a, const int* b) {
+    struct prox *a1 = (struct prox *)a;
+    struct prox *a2 = (struct prox *)b;
+    if ((*a1).value > (*a2).value)
+        return -1;
+    else if ((*a1).value < (*a2).value)
+        return 1;
+    else
+        return 0;
+}
+
+/* Direction of turn.
+ * 		1 : clockwise (right)
+ * 		-1 : counterclockwise (left)
+ */
+int get_turn_direction() {
+	return target_turn_angle < 0 ? -1.0 : 1.0;
+}
+
+/*********** Obstacle Detection *********************/
+
+/*	1 : obstacle in proximity
+ *  0 : no obstacle in proximity
+ */
+int obstacle_in_proximity(void) {
+	const int thr = 500;	// threshold value
+	int obstacle_detected = 0;
+
+	for(int sensor = 0; sensor < 8; sensor++) {
+		if(get_prox(sensor) > thr) {
+			obstacle_detected = 1;
+		}
+	}
+	return obstacle_detected;
+}
+
+/******************* TASK 2 ADDITIONAL HELPERS ******************/
+
+/*	1 : target in range
+ *  0 : target not in range
+ */
+int target_in_range(void) {
+	const int thr = 500;	// threshold value
+	int target_detected = 0;
+
+	for(int sensor = 0; sensor < 8; sensor++) {
+		if(get_prox(sensor) < thr) {
+			target_detected = 1;
+		}
+	}
+	return target_detected;
+}
+
+/*************** Feedback **************************/
+
+// Send data to the terminal
+void send_feedback_data(void) {
+
+	// Prox sensor values:
+	for(int sensor = 0; sensor < 8; sensor ++ ) {
+		int prox_value = get_prox(sensor);
+		char str[100]; // resulting string of sprintf will be stored here
+		char split = (sensor == 7) ? '\n' : '|';
+		int str_length = sprintf(str, " s%d: %d %c", sensor, prox_value, split);
+		e_send_uart1_char(str, str_length);
+	}
+
+	// Gyro values:
+	char str[100]; // resulting string of sprintf will be stored here
+	int str_length = sprintf(str, "current angle: %f, target angle: %f \n", current_turn_angle, target_turn_angle);
+	e_send_uart1_char(str, str_length);
+
+}
